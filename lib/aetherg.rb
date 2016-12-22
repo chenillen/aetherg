9122811bb5f9e011161f5160ace1912ec9c4244b
chenillen <chenillen@gmail.com>
2016-12-22 11:24:24 +0800

    add aetherg --version action

--- lib/aetherg.rb
+++ lib/aetherg.rb
@@ -2,3 +2,4 @@ require 'rubygems'
 require 'thor/group'
 require File.expand_path('../aetherg/string', __FILE__)
 require File.expand_path('../aetherg/aetherg', __FILE__)
+require File.expand_path('../aetherg/version', __FILE__)
---
+++ lib/aetherg/version.rb
@@ -0,0 +1,4 @@
+module Aetherg
+  VERSION = "0.3.0"
+  BUILD   = "2016-12-22"
+end
